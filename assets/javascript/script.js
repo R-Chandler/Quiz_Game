f16ecc577ad7d79f4ab891d3f73fb303e9a06543
Ross Chandler <rosspchandler@gmail.com>
2024-01-07 16:12:23 +0000

    add first question and link to html elements

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,14 @@
-console.log("Hello!")
+
+
+/** Questions */
+
+var question1 = {
+    question: "What is the capital of Australia?",
+    choices: ["Sydney", "Melbourne", "Canberra"],
+    answer: "Canberra",
+};
+
+document.getElementById("currentQuestion").innerText = question1.question;
+document.getElementById("answerOne").innerText = question1.choices[0];
+document.getElementById("answerTwo").innerText = question1.choices[1];
+document.getElementById("answerThree").innerText = question1.choices[2];
